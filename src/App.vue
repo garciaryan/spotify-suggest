ee8d52d057dca7008e900733cf67bac745677ccf
Ryan Garcia <garciaryan007@yahoo.com>
2019-01-12 15:18:23 -0800

    dark theme and title bar

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,11 @@
 <template>
-  <v-app>
+  <v-app dark>
     <v-toolbar app>
+      <v-img :src="require('./assets/Spotify_Logo_RGB_Green.png')" height="45" max-width="120" contain />
       <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+        <span class="font-weight-light">Suggest</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
     </v-toolbar>
 
     <v-content>
