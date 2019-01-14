c78024df00678f420f4c60e60ff5b4002c39ac72
Ryan Garcia <garciaryan007@yahoo.com>
2019-01-14 15:15:04 -0800

    starting to work with track items

--- src/components/Suggestions.vue
+++ src/components/Suggestions.vue
@@ -14,45 +14,37 @@
       </v-toolbar>
 
       <v-list subheader>
-        <v-subheader>Recent chat</v-subheader>
+        <v-subheader>Check out these suggested tracks!</v-subheader>
         <v-list-tile
-          v-for="item in items"
-          :key="item.title"
+          v-for="track in tracks"
+          :key="track.id"
           avatar
         >
           <v-list-tile-avatar>
-            <img :src="item.avatar">
+            <img :src="track.images.url">
           </v-list-tile-avatar>
 
           <v-list-tile-content>
-            <v-list-tile-title v-html="item.title"></v-list-tile-title>
+            <v-list-tile-title v-html="track.name"></v-list-tile-title>
           </v-list-tile-content>
 
           <v-list-tile-action>
-            <v-icon :color="item.active ? 'teal' : 'grey'">chat_bubble</v-icon>
+            <v-icon>play_circle_filled</v-icon>
           </v-list-tile-action>
-        </v-list-tile>
-      </v-list>
-
-      <v-divider></v-divider>
-
-      <v-list subheader>
-        <v-subheader>Previous chats</v-subheader>
-
-        <v-list-tile
-          v-for="item in items2"
-          :key="item.title"
-          avatar
-        >
-          <v-list-tile-avatar>
-            <img :src="item.avatar">
-          </v-list-tile-avatar>
-
-          <v-list-tile-content>
-            <v-list-tile-title v-html="item.title"></v-list-tile-title>
-          </v-list-tile-content>
         </v-list-tile>
       </v-list>
     </v-card>
   </v-flex>
 </template>
+
+<script>
+export default {
+  data(){
+    return {
+      tracks: [],
+
+    }
+  }
+}
+</script>
+
