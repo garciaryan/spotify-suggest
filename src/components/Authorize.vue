0ccb65e5573423844833797e58d8c56f73ff13de
Ryan Garcia <garciaryan007@yahoo.com>
2019-01-18 12:52:21 -0800

    working out control flow for spotify auth request

--- src/components/Authorize.vue
+++ src/components/Authorize.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-layout row justify-center @click='accessToken'>
+  <v-layout row justify-center>
     <v-btn color="#1DB954" large :href=url>Log in with Spotify</v-btn>
   </v-layout>
 </template>
@@ -16,23 +16,27 @@ export default {
     }
   },
 
-  methods: {
+  computed: {
     accessToken(){
-      let clientId = process.env.VUE_APP_CLIENT_ID,
-        code = this.code,
-        secret = process.env.VUE_APP_CLIENT_SECRET;
+      if (this.$store.state.loggedIn === true){
+        let clientId = process.env.VUE_APP_CLIENT_ID,
+          code = this.code,
+          secret = process.env.VUE_APP_CLIENT_SECRET;
 
-      axios.post('https://accounts.spotify.com/api/token', {
-        grant_type: 'authorization_code',
-        code: code,
-        redirect_uri: 'http:/localhost:8080/callback',
-        client_id: clientId, 
-        client_secret: secret 
-      }, {withCredentials: true}).then(res => {
-        console.log(res);
-      }).catch(err => {
-        console.log(err);
-      })
+        axios.post('https://accounts.spotify.com/api/token', {
+          grant_type: 'authorization_code',
+          code: code,
+          redirect_uri: 'http:/localhost:8080/callback',
+          client_id: clientId, 
+          client_secret: secret 
+        }, {withCredentials: true}).then(res => {
+          console.log(res);
+        }).catch(err => {
+          console.log(err);
+        })
+      } else {
+        console.log('not logged in');
+      }
     }
   }
 }
